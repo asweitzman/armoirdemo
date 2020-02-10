ecc35de9ddd543611bc975631267ecffdcd7aa02
Alex Weitzman <asweitzman@gmail.com>
2020-02-09 21:14:53 -0800

    added gitignore

--- Pods/Target Support Files/Pods-Armoir/Pods-Armoir-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Armoir : NSObject
-@end
-@implementation PodsDummy_Pods_Armoir
-@end
--- Pods/Target Support Files/Pods-ArmoirTests/Pods-ArmoirTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ArmoirTests : NSObject
-@end
-@implementation PodsDummy_Pods_ArmoirTests
-@end
