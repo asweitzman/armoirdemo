e2e95486382d8b1ad9be725cf614bd9a3f5f5107
Alex Weitzman <asweitzman@gmail.com>
2020-02-10 13:25:19 -0800

    fixed button in add item

--- Armoir/AddItemViewController.swift
+++ Armoir/AddItemViewController.swift
@@ -57,207 +57,206 @@ class AddItemViewController: UIViewController, UITextFieldDelegate {
       return String((0..<length).map{ _ in letters.randomElement()! })
     }
     
-//    @IBAction func buttonTapped(_ sender: UIButton) {
-//        let storage = Storage.storage()
-//        let storageRef = storage.reference()
-//        let ref = Database.database().reference()
-//        var user = Auth.auth().currentUser
-//        Analytics.logEvent("add_item_button_pressed", parameters: ["item" : "pressed"])
-//
-//        if (Description.text == "" || Price.text == "" || categoryButton.titleLabel!.text == "Category" || sizeButton.titleLabel!.text == "Size") {
-//
-//            missingDetailsLabel.isHidden = false
-//        } else {
-//
-//        //let description: String = Description.text!
-//        //needs to be a double based on what they enter
-//            var imageURL = ""
-//            let imageID = randomString(length:8)
-//            let imageRef = storageRef.child("images/" + imageID);
-//            var imageData = Data()
-//            imageData = itemImage.jpegData(compressionQuality: 0.8)!
-//            let uploadTask = imageRef.putData(imageData, metadata: nil) { (metadata, error) in
-//              guard let metadata = metadata else {
-//                // Uh-oh, an error occurred!
-//                return
-//              }
-//            }
-//
-//        //if (!startWithCamera) {
-//            ImageRetriever().save(image: itemImage);
-//            imageURL = ImageRetriever().loadStr(fileName: "SavedImage" + String(numImgSaved))
-//            print(ImageRetriever().fileIsURL(fileName: imageURL))
-//        //}
-//
-////        print("URL: " + imageURL)
-//            /*if let price = Double(price.text) {
-//
-//            } else {
-//
-//            }*/
-//        let price = Price.text!
-//        let priceDouble = Double(price)!
-//        let description = Description.text!
-//        var numItems = 0
-//        let category = "shirt"
-//        for u in all_users {
-//            for i in u.closet {
-//                numItems += 1
-//            }
-//        }
-//
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        /*
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let ref = Database.database().reference()
+        var user = Auth.auth().currentUser
+        Analytics.logEvent("add_item_button_pressed", parameters: ["item" : "pressed"])
+*/
+        if (Description.text == "" || Price.text == "" || categoryButton.titleLabel!.text == "Category" || sizeButton.titleLabel!.text == "Size") {
+
+            missingDetailsLabel.isHidden = false
+        } else {
+
+        //let description: String = Description.text!
+        //needs to be a double based on what they enter
+
+         var imageURL = ""
+        /*
+            let imageID = randomString(length:8)
+            let imageRef = storageRef.child("images/" + imageID);
+            var imageData = Data()
+            imageData = itemImage.jpegData(compressionQuality: 0.8)!
+            let uploadTask = imageRef.putData(imageData, metadata: nil) { (metadata, error) in
+              guard let metadata = metadata else {
+                // Uh-oh, an error occurred!
+                return
+              }
+            }
+*/
+        //if (!startWithCamera) {
+            ImageRetriever().save(image: itemImage);
+            imageURL = ImageRetriever().loadStr(fileName: "SavedImage" + String(numImgSaved))
+            print(ImageRetriever().fileIsURL(fileName: imageURL))
+        //}
+        
+
+//        print("URL: " + imageURL)
+            /*if let price = Double(price.text) {
+
+            } else {
+
+            }*/
+        let price = Price.text!
+        let priceDouble = Double(price)!
+        let description = Description.text!
+        var numItems = 0
+        let category = "shirt"
+        for u in all_users {
+            for i in u.closet {
+                numItems += 1
+            }
+        }
+
 //        if (imageID != "") {}
-//            let new_item = Item(item_id: numItems+1, name: description, owner: currUser.user_ID, borrowed: false, borrowed_by: 0, image: imageURL, color: "", size: itemSize, price: priceDouble, category: itemCategory)
-//
-//            //save to firebase database
+        
+       // if (imageURL != "") {}
+            let new_item = Item(item_id: numItems+1, name: description, owner: currUser.user_ID, borrowed: false, borrowed_by: 0, image: imageURL, color: "", size: itemSize, price: priceDouble, category: itemCategory)
+        
+
+            //save to firebase database
 //            let itemID = imageID
 //            ref.child("items").child(itemID).setValue(["name": description, "owner": user?.uid, "borrowed": false, "borrowed_by": 0, "image": imageID, "color": "", "size": itemSize, "price": priceDouble, "category": itemCategory])
 //            var currItemsRef = ref.child("users").child(user!.uid).child("closet")
 //            ref.child("users/\(user!.uid)/closet/\(itemID)/").setValue(true)
+
+        //1. find index of currUser in all_users array
+        var i = 0;
+        var found = false;
+        for u in all_users {
+            if (u.user_ID == currUser.user_ID) {
+                found = true
+            }
+            if (!found) {
+                i += 1
+            }
+        }
+        //2. use the index to change the actual element in all users
+        //print (all_users[i]) //testing before
+ 
+        /*var temp = all_users[i].closet
+         
+        temp.append(new_item)
+        all_users[i].closet = temp
+        print(all_users[i]) // testing after*/
+/*
+        //to check if all_users updated
+        //print(all_users)
+
+        //encode to json
+        var text = "" //just a text
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        do {
+            let data = try encoder.encode(all_users)
+            text = String(data: data, encoding: .utf8)!
+            print("DONE ENCODING")
+            //print(String(data: data, encoding: .utf8)!)
+        }
+        catch {
+            print("array didn't work");
+        }
+
+
+        //let fileName = "search.json"
+            //let filePath = documentDirectory.appendingPathComponent("search.json").absoluteString
+
+           // let filePath = (documentDirectory as NSString).stringByAppendingPathComponent("search.json")
+           // let filePath = self.applicationDocumentsDirectory().path?.stringByAppendingString(fileName)
+
+            do {
+                try text.write(toFile: fullDestPathString, atomically: true, encoding: String.Encoding.utf8)
+                print(fullDestPathString)
+            }
+            catch {
+                print(error)
+            }
+
+        //
+        /*let path = "search" //this is the file. we will write to and read from it
+        print("continuing");
+
+        if let fileURL = Bundle.main.url(forResource: path, withExtension: "json") {
+
+            do {
+                try text.write(to: fileURL, atomically: false, encoding: .utf8)
+                print("tried to write")
+            }
+            catch {
+                print(error)
+            }
+        }*/
+
+
+
+//            let from = Bundle.main.url(forResource: "search", withExtension: "json")!
 //
-//        //1. find index of currUser in all_users array
-//        var i = 0;
-//        var found = false;
-//        for u in all_users {
-//            if (u.user_ID == currUser.user_ID) {
-//                found = true
-//            }
-//            if (!found) {
-//                i += 1
-//            }
-//        }
-//        //2. use the index to change the actual element in all users
-//        //print (all_users[i]) //testing before
-////        var temp = all_users[i].closet
-////        temp.append(new_item)
-////        all_users[i].closet = temp
-////        print(all_users[i]) // testing after
-//
-//        //to check if all_users updated
-//        //print(all_users)
-//
-//        //encode to json
-//        var text = "" //just a text
-//        let encoder = JSONEncoder()
-//        encoder.outputFormatting = .prettyPrinted
-//        do {
-//            let data = try encoder.encode(all_users)
-//            text = String(data: data, encoding: .utf8)!
-//            print("DONE ENCODING")
-//            //print(String(data: data, encoding: .utf8)!)
-//        }
-//        catch {
-//            print("array didn't work");
-//        }
-//
-//
-//        //let fileName = "search.json"
-//            //let filePath = documentDirectory.appendingPathComponent("search.json").absoluteString
-//
-//           // let filePath = (documentDirectory as NSString).stringByAppendingPathComponent("search.json")
-//           // let filePath = self.applicationDocumentsDirectory().path?.stringByAppendingString(fileName)
+//            let to = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("result.json")
 //
 //            do {
-//                try text.write(toFile: fullDestPathString, atomically: true, encoding: String.Encoding.utf8)
-//                print(fullDestPathString)
+//
+//                try FileManager.default.copyItem(at: from, to: to)
+//
+//                print(try FileManager.default.contents(atPath: to.path))
+//
+//                let wer = Data("rerree".utf8 )
+//
+//                try wer.write(to: to)
+//
+//                print(try FileManager.default.contents(atPath: to.path))
+//
 //            }
 //            catch {
+//
 //                print(error)
 //            }
 //
-//        //
-//        /*let path = "search" //this is the file. we will write to and read from it
-//        print("continuing");
-//
-//        if let fileURL = Bundle.main.url(forResource: path, withExtension: "json") {
-//
-//            do {
-//                try text.write(to: fileURL, atomically: false, encoding: .utf8)
-//                print("tried to write")
-//            }
-//            catch {
-//                print(error)
-//            }
-//        }*/
-//
-//
-//
-////            let from = Bundle.main.url(forResource: "search", withExtension: "json")!
-////
-////            let to = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("result.json")
-////
-////            do {
-////
-////                try FileManager.default.copyItem(at: from, to: to)
-////
-////                print(try FileManager.default.contents(atPath: to.path))
-////
-////                let wer = Data("rerree".utf8 )
-////
-////                try wer.write(to: to)
-////
-////                print(try FileManager.default.contents(atPath: to.path))
-////
-////            }
-////            catch {
-////
-////                print(error)
-////            }
-////
-////            let fileName = "search"
-////            let documentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-////            let fileURL = documentDirURL.appendingPathComponent(fileName).appendingPathExtension("json")
-////                print("File PAth: \(fileURL.path)")
-//
-//
-//        /*
-//        let new_item = Item(item_id: numItems+1, name: "Jean Jacket", owner: currUser.user_ID, borrowed: false, borrowed_by: 0, image: "jeanJacketFinal", color: "red", size: "M", price: price, category: category)
-//
-//        /*let new_item = Item(item_id: numItems+1, name: description, owner: currUser.user_ID, borrowed: false, borrowed_by: 0, image: imageURL, color: color, size: "S", price: price, category: "shirt")*/
-//
-//        for var u in all_users {
-//            if (u.user_ID == currUser.user_ID) {
-//                u.closet.append(new_item)
-//            }
-//        }
-//        */
-//        //currUser.closet.append(new_item)
-//        numImgSaved += 1
-//
-//        //if (startWithCamera) {
-//           // print("true")
-//            // Go back two ViewControllers
-//            //_ = navigationController?.popViewControllers(viewsToPop: 1)
-//        //} else {
-//            _ = navigationController?.popViewControllers(viewsToPop: 1)
-//        //}
-//        }
-//    }
-//
+//            let fileName = "search"
+//            let documentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+//            let fileURL = documentDirURL.appendingPathComponent(fileName).appendingPathExtension("json")
+//                print("File PAth: \(fileURL.path)")
+
+
+        
+        let new_item = Item(item_id: numItems+1, name: "Jean Jacket", owner: currUser.user_ID, borrowed: false, borrowed_by: 0, image: "jeanJacketFinal", color: "red", size: "M", price: price, category: category)
+
+  */
+ /*let new_item = Item(item_id: numItems+1, name: description, owner: currUser.user_ID, borrowed: false, borrowed_by: 0, image: imageURL, color: color, size: "S", price: price, category: "shirt")
+*/
+        for var u in all_users {
+            if (u.user_ID == currUser.user_ID) {
+                u.closet.append(new_item)
+            }
+        }
+        
+        //currUser.closet.append(new_item)
+        numImgSaved += 1
+
+
+        //if (startWithCamera) {
+           // print("true")
+              //Go back two ViewControllers
+            //_ = navigationController?.popViewControllers(viewsToPop: 1)
+        //} else {
+            _ = navigationController?.popViewControllers(viewsToPop: 1)
+        //}
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.Description.delegate = self
         self.Description.returnKeyType = UIReturnKeyType.done
         self.Description.autocorrectionType = .no
         missingDetailsLabel.isHidden = true
-//        let icon = UIImage(named: "downarrow3")!
-//        categoryButton.setImage(icon, for: .normal)
-//        categoryButton.imageView?.contentMode = .scaleAspectFit
-//        categoryButton.semanticContentAttribute = UIApplication.shared
-//            .userInterfaceLayoutDirection == .rightToLeft ? .forceLeftToRight : .forceRightToLeft
-//        categoryButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: categoryButton.frame.size.width - categoryButton.titleLabel!.intrinsicContentSize.width, bottom: 0, right: 0)
-//        sizeButton.setImage(icon, for: .normal)
-//        sizeButton.imageView?.contentMode = .scaleAspectFit
-//        sizeButton.semanticContentAttribute = UIApplication.shared
-//            .userInterfaceLayoutDirection == .rightToLeft ? .forceLeftToRight : .forceRightToLeft
-//        sizeButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: sizeButton.frame.size.width - sizeButton.titleLabel!.intrinsicContentSize.width, bottom: 0, right: 0)
         itemCategory = ""
         itemSize = ""
         
         initDropDowns()
         initcategoryDropDown2()
         initSizeDropDown()
-        
         itemImageView.image = itemImage
         self.navigationItem.hidesBackButton = true
         let newBackButton = UIBarButtonItem(title: "Back", style: UIBarButtonItem.Style.done, target: self, action: #selector(self.back(sender:)))
